34e1ef0c76710756253fdca020cc8f1087b2fe43
Fantonos <galaxxy@sudomail.com>
2024-10-18 02:14:30 -0400

    Completed V0.05

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import Shot
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
+   
     game_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    game_loop(screen, clock, game_player)
-    print("Starting asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}') 
-    print(f'Screen height: {SCREEN_HEIGHT}')
 
-
-def game_loop(screen, clock, player):
     while True:
         for event in pygame.event.get():# This below will check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000 # dt is the time between frames
-        player.rotate(dt)
+        
+        for obj in updatable:
+            obj.update(dt)    
+        
+        for asteroid in asteroids:
+            #check for collision with bullet
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    shot.kill()
+                    asteroid.split()
+                    break
+
+            #check for collision with player
+            if asteroid.check_collision(game_player):
+                print("Game Over")
+                exit()
+                
+        game_player.PLAYER_SHOOT_COOLDOWN -= dt
+             
         screen.fill("black")
-        player.draw(screen)
-        pygame.display.update()
-
         
+        for obj in drawable:
+            obj.draw(screen)
+            
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000 # dt is the time between frames
         
-
-
-
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 import pygame
 
 '''
@@ -17,6 +18,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.PLAYER_SHOOT_COOLDOWN = 0 
     
     # in the player class
     def triangle(self):
@@ -30,12 +32,30 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
         
-    def rotate(self, dt):
+    def update(self, dt):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            print("left")
-            self.rotation += PLAYER_TURN_SPEED * dt
         if keys[pygame.K_d]:
-            print("right")
+            self.rotation += PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_a]:
             self.rotation -= PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.position -= pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SPEED * dt
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt  
+    
+    def shoot(self):
+        if self.PLAYER_SHOOT_COOLDOWN > 0:
+            return
+        self.PLAYER_SHOOT_COOLDOWN = PLAYER_SHOOT_COOLDOWN  
+        shot = Shot(self.position, self.rotation, SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        
+        
+        
     
